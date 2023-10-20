7db2f35ee11431141c6a35e1c8c4963aa56d583d
674RoMaN <tauhidroman@gmail.com>
2023-10-20 15:41:20 +0600

    responsive done

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
 
         <>
             <div className="mx-10 rounded-2xl my-10">
-                <div className="lg:h-[750px] text-white text-center md:h-[460px]  grid h-[230px] bg-contain lg:bg-cover bg-no-repeat bg-[url('https://i.ibb.co/zG6BC5y/banner-1.jpg')] mb-8">
+                <div className="lg:h-[750px] text-white text-center md:h-[460px]  grid h-[160px] bg-contain lg:bg-cover bg-no-repeat bg-[url('https://i.ibb.co/zG6BC5y/banner-1.jpg')] mb-8">
                     <div className="col-start-1 row-start-1 bg-black bg-opacity-60 w-full h-full"></div>
                     <div className="col-start-1 row-start-1 mx-auto my-auto">
                         <div>
